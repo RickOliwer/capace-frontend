d1d92f16fb5f987d3a1bc3485fc0e040e74960d2
RickOliwer <rickoliwer@gmail.com>
2022-01-18 22:33:46 +0100

    rendering out content for team

--- src/components/pages/about/team-module/index.js
+++ src/components/pages/about/team-module/index.js
@@ -1,14 +1,44 @@
+import Image from 'next/image'
 const AboutTeam = ({ team }) => {
-    console.log('the team', team);
+    
     return ( 
         <div>
             <div>
-                {team?.aboutTeam?.map((title) => {
-                    return <h2>{title?.heading} <span className="capace-oranges">.</span></h2>
+                {team?.aboutTeam?.map((title, index) => {
+                    return <h2 key={`${index}teamTitle`}>{title?.heading} <span className="capace-oranges">.</span></h2>
                 })}
             </div>
-            <div>
+            <div className='lg:grid lg:grid-cols-2 gap-10'>
+                {
+                    team?.aboutTeam?.map((teams) => {
+                        return teams?.team?.map((post) => {
+                            
+                            return post?.GQL_employeesContent?.employeeIntro?.map((emp, index) => {
+                                console.log(post);
+                                return <div className='team-card' key={post?.id}>
+                                
+                                            
+                                            <div className="relative h-screen">
+                                                <Image 
+                                                    layout="fill"
+                                                    objectFit='cover'
+                                                    src={emp?.image?.mediaItemUrl}
+                                                />
+                                            </div>
+                                            
+                                            <div>
+                                                <p className='capace-oranges'>{emp?.role}</p>
+                                                <p>{emp?.namn} <span className='capace-oranges'>.</span></p>
+                                                <p className='capace-oranges'>{emp?.email}</p>
 
+                                            </div>
+                                    
+                                       </div>
+                                
+                            })
+                        })
+                    })
+                }
             </div>
         </div>
     );
