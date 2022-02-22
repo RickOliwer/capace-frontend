5878c3a69155142f07862c679c8e04857cd6d1bb
RickOliwer <rickoliwer@gmail.com>
2022-02-22 08:14:54 +0100

    serv-post working on blocks

--- src/components/Blocks/sections/intro.js
+++ src/components/Blocks/sections/intro.js
@@ -11,26 +11,26 @@ const Intro = ( { content } ) => {
         }
     }, [content?.textGrid, setGrid])
     return (
-        <div className={`layout-top ${isGrid ? 'grid grid-cols-3 gap-10' : ''}`}>
-            <div>
+        <div className={`layout-top intro-block ${isGrid ? 'grid grid-cols-3 gap-10' : ''}`}>
+            <div className={``}>
                 <h2 className="flex flex-col">
-                    <span className="mb-2 text-3xl lg:text-4xl">{content?.rubrik1}</span>
+                    <span className="mb-2 text-2xl lg:text-3xl">{content?.rubrik1}</span>
                     <span className="text-4xl lg:text-6xl">{content?.rubrik2}<span className="capace-oranges">.</span></span>
                 </h2>
 
                 <p>{content?.utdrag}</p>
             </div>
             {!isEmpty(content?.textGrid) && (
-                <div className="grid grid-cols-2 intro-grid">
+                <div className="grid grid-cols-2 col-span-2 gap-10 intro-grid">
                     {content?.textGrid?.map((grid, index) => {
                         return (
-                            <div key={`#€"%%${index}${grid?.text}`}>
+                            <div className="grid-item" key={`#€"%%${index}${grid?.text}`}>
                                 {!isEmpty(grid?.rubrik) && (
                                     <h3>{grid?.rubrik}</h3>
                                 )}
                                 <p>{grid?.text}</p>
-                                {!isEmpty(grid?.avslute) && (
-                                    <p>{grid?.text}</p>
+                                {!isEmpty(grid?.avslut) && (
+                                    <p className="mt-4">{grid?.avslut}</p>
                                 )}
                             </div>
                         )
