43642e1cead15c99abd867d249021a172efb9880
RickOliwer <rickoliwer@gmail.com>
2022-04-04 10:33:10 +0200

    setting up instagram feed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 
 import Client from '../src/apollo/client';
 import Blocks from '../src/components/Blocks';
+import InstaFeeds from '../src/components/instaFeeds';
 import Hero from '../src/components/layout/header/hero';
 import { GET_PAGE } from '../src/queries/pages/get-page';
 import { handleRedirectsAndReturnData } from '../src/utils/slugs';
@@ -17,7 +18,7 @@ export default function Home({ data }) {
       <Hero hero={data?.page} logo={data?.header?.siteLogoUrl} />
 
       <Blocks block={data?.page?.GQL_Flexible_Content} pageTitle={data?.page?.title}/>
-
+      <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} />
     </>
   )
 }
