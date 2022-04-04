fb4210494274c18c78c63ebd607ba8e638dcb406
RickOliwer <rickoliwer@gmail.com>
2022-04-04 13:16:54 +0200

    instafeed testing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 
 import Client from '../src/apollo/client';
 import Blocks from '../src/components/Blocks';
-import InstaFeeds from '../src/components/instagram/InstaFeeds';
+import getInstaFeed from '../src/components/instagram/InstaFeeds';
+//import InstaFeeds from '../src/components/instagram/InstaFeeds';
 import Hero from '../src/components/layout/header/hero';
 import { GET_PAGE } from '../src/queries/pages/get-page';
 import { handleRedirectsAndReturnData } from '../src/utils/slugs';
@@ -9,16 +10,54 @@ import { handleRedirectsAndReturnData } from '../src/utils/slugs';
 
 
 
-export default function Home({ data }) {
+export default function Home({ data, instagram }) {
 
+  console.log('instagram feed', instagram.data);
   
+       
   return (
     <>
      
       <Hero hero={data?.page} logo={data?.header?.siteLogoUrl} />
 
       <Blocks block={data?.page?.GQL_Flexible_Content} pageTitle={data?.page?.title}/>
-      <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} />
+      <div className='container layout contain layout-top'>
+        {instagram?.data?.map((feed) => {
+          if(feed?.media_type === "VIDEO"){
+            return (
+              <video
+                width='100%'
+                height='auto' 
+                src={feed?.media_url} 
+                type="video/mp4" 
+                controls playsinline>
+              </video>
+            )
+          }else if(feed?.media_type === "CAROUSEL_ALBUM"){
+            return (
+              <img 
+                width='100%'
+                height='auto'
+                id={feed?.id} 
+                src={feed?.media_url} 
+                alt={feed?.caption} 
+              />
+            )
+          } else {
+            return (
+
+              <img 
+                  width='100%'
+                  height='auto'
+                  id={feed?.id} 
+                  src={feed?.media_url} 
+                  alt={feed?.caption} 
+              />
+            )
+          }
+        })}
+      </div>
+      {/* <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} /> */}
     </>
   )
 }
@@ -31,11 +70,14 @@ export async function getStaticProps(){
       uri: '/',
     }
   })
+
+  const instagram = await getInstaFeed()
    const defaultProps = {
     props: {
       data: data || {},
+      instagram,
     },
-    revalidate: 1,
+    revalidate: 30,
   }
 
   return handleRedirectsAndReturnData(defaultProps, data, errors, 'page')
--- src/components/instagram/InstaFeeds.js
+++ src/components/instagram/InstaFeeds.js
@@ -1,57 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
-import Feed from './Feed'
-const InstaFeeds = ({token, ...props}) => {
-    const [feeds, setFeedsData] = useState([]);
-    //use useRef to store the latest value of the prop without firing the effect
-    const tokenProp = useRef(token);
-    tokenProp.current = token;
-    console.log('the token ====>', token);
-
-    useEffect(() => {
-        // this is to avoid memory leaks
-        const abortController = new AbortController();
-        console.log('URL ===>', `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${tokenProp.current}`);
-        async function fetchInstagramPost () {
-            let config = {
-                method: 'get',
-                url: `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${tokenProp.current}`,
-                responseType: 'stream'
-            }
-            try{
-                // axios
-                //     .get(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${props.limit}&access_token=${tokenProp.current}`)
-                //     .then((resp) => {
-                //         setFeedsData(resp.data)
-                //     })
-                await axios(config).then(function (response) {
-                    console.log('my response ===>',response);
-                })
-            } catch (err) {
-                console.log('error', err)
-            }
-        }
-
-        // manually call the fecth function 
-        fetchInstagramPost();
-        console.log('FEEDS AFTER TRY CATCH ===>', feeds);
-        return () => {
-            // cancel pending fetch request on component unmount
-            abortController.abort(); 
-        };
-    }, [props.limit])
-
-    return (
-        <div className="container">
-            {feeds.map((feed) => {
-                console.log('feed component ===>', feed)
-                return (
-                    <Feed key={feed.id} feed={feed} />
-                )
-            })}
-        </div>
-    );
+const getInstaFeed = async () => {
+    try {
+        const res = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=12&access_token=${process.env.REACT_APP_INS_TOKEN}`)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.error("ERROR =>",error)
+    }
 }
-
-export default InstaFeeds;
-
+ 
+export default getInstaFeed;
