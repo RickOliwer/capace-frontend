516767818c9419bb578058c7dec989d28573d13d
RickOliwer <rickoliwer@gmail.com>
2022-04-04 16:54:21 +0200

    instafeed done

--- pages/index.js
+++ pages/index.js
@@ -2,62 +2,72 @@
 import Client from '../src/apollo/client';
 import Blocks from '../src/components/Blocks';
 import getInstaFeed from '../src/components/instagram/InstaFeeds';
-//import InstaFeeds from '../src/components/instagram/InstaFeeds';
 import Hero from '../src/components/layout/header/hero';
 import { GET_PAGE } from '../src/queries/pages/get-page';
 import { handleRedirectsAndReturnData } from '../src/utils/slugs';
 
 
-
-
 export default function Home({ data, instagram }) {
 
-  console.log('instagram feed', instagram.data);
   
-       
+
   return (
     <>
      
       <Hero hero={data?.page} logo={data?.header?.siteLogoUrl} />
 
       <Blocks block={data?.page?.GQL_Flexible_Content} pageTitle={data?.page?.title}/>
-      <div className='container layout contain layout-top'>
-        {instagram?.data?.map((feed) => {
-          if(feed?.media_type === "VIDEO"){
-            return (
-              <video
-                width='100%'
-                height='auto' 
-                src={feed?.media_url} 
-                type="video/mp4" 
-                controls playsinline>
-              </video>
-            )
-          }else if(feed?.media_type === "CAROUSEL_ALBUM"){
-            return (
-              <img 
-                width='100%'
-                height='auto'
-                id={feed?.id} 
-                src={feed?.media_url} 
-                alt={feed?.caption} 
-              />
-            )
-          } else {
-            return (
+      <div className='layout contain layout-top'>
+        <div className='mb-7'>
+          <h2 className="text-5xl uppercase lg:text-7xl">instagram<span className="text-capace-oranges">.</span></h2>
+          <a className='hover:text-capace-oranges' rel="noreferrer" target="_blank" href="https://www.instagram.com/capace_media/?hl=sv">@capace_media</a>
 
-              <img 
-                  width='100%'
-                  height='auto'
-                  id={feed?.id} 
-                  src={feed?.media_url} 
-                  alt={feed?.caption} 
-              />
-            )
-          }
-        })}
+        </div>
+        <div className='container'>
+          {instagram?.data?.map((feed) => {
+            if(feed?.media_type === "VIDEO"){
+              return (
+                <div key={feed?.id}>
+
+                  <video
+                    width='100%'
+                    height='auto' 
+                    src={feed?.media_url} 
+                    type="video/mp4" 
+                    controls playsinline>
+                  </video>
+                </div>
+              )
+            }else if(feed?.media_type === "CAROUSEL_ALBUM"){
+              return (
+                <div key={feed?.id}>
+
+                  <img 
+                    width='100%'
+                    height='auto'
+                    id={feed?.id} 
+                    src={feed?.media_url} 
+                    alt={feed?.caption} 
+                  />
+                </div>
+              )
+            } else {
+              return (
+                <div key={feed?.id}>
+
+                  <img 
+                      width='100%'
+                      height='auto'
+                      id={feed?.id} 
+                      src={feed?.media_url} 
+                      alt={feed?.caption} 
+                  />
+                </div>
+              )
+            }
+          })}
+        </div> 
       </div>
-      {/* <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12} /> */}
     </>
   )
 }
--- src/components/instagram/InstaFeeds.js
+++ src/components/instagram/InstaFeeds.js
@@ -1,5 +1,8 @@
+
+
 const getInstaFeed = async () => {
     try {
+
         const res = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=12&access_token=${process.env.REACT_APP_INS_TOKEN}`)
         const data = await res.json()
         return data
